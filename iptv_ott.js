cebc3a0d3bd7426772c5c9847a53efdf45d58595
Artrax90 <67760877+Artrax90@users.noreply.github.com>
2026-02-07 19:49:17 +0300

    Update iptv_ott.js

--- iptv_ott.js
+++ iptv_ott.js
@@ -1,150 +1,202 @@
 // ==Lampa==
-// name: IPTV OTT
-// version: 1.0.1
-// description: IPTV plugin (OttPlayer style)
-// author: Artrax90
+// name: Lampa IPTV Lite
+// version: 1.0.0
+// description: Простой IPTV плеер для Lampa с поддержкой M3U и EPG
+// author: Gemini Developer
 // ==/Lampa==
 
 (function () {
     'use strict';
-    if (!window.Lampa) return;
 
+    function IPTVPlugin() {
+        var network = new Lampa.Reguest();
+        var storage = Lampa.Storage;
+        var playlistData = [];
+        var groups = {};
+
+        this.init = function () {
+            console.log('IPTV Plugin: Init start');
+
+            // Регистрация компонента экрана
+            Lampa.Component.add('iptv_lite', this.render.bind(this));
+
+            // Добавление пункта в настройки
+            Lampa.Settings.add({
+                title: 'IPTV Lite',
+                type: 'button',
+                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M21 7V5a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2zM5 7V5h14v2zm14 12H5a2 2 0 0 0-2 2v2h18v-2a2 2 0 0 0-2-2zm0 2H5v-2h14zm-9-5h4a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2zm0-5h4v3h-4z" fill="white"/></svg>',
+                onContext: function () {
+                    Lampa.Select.show({
+                        title: 'IPTV Lite',
+                        items: [
+                            {title: 'Открыть плеер', action: 'open'},
+                            {title: 'Настроить ссылки', action: 'settings'}
+                        ],
+                        onSelect: function (item) {
+                            if (item.action === 'open') Lampa.Activity.push({url: '', title: 'IPTV Lite', component: 'iptv_lite', page: 1});
+                            else Lampa.Settings.main('iptv_lite_settings');
+                        }
+                    });
+                }
+            });
+
+            // Создание страницы настроек
+            Lampa.Settings.main && Lampa.Settings.main({
+                id: 'iptv_lite_settings',
+                title: 'IPTV Lite Настройки',
+                icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M19.43 12.98c.04-.32.07-.64.07-.98s-.03-.66-.07-.98l2.11-1.65c.19-.15.24-.42.12-.64l-2-3.46c-.12-.22-.39-.3-.61-.22l-2.49 1c-.52-.4-1.08-.73-1.69-.98l-.38-2.65C14.46 2.18 14.25 2 14 2h-4c-.25 0-.46.18-.49.42l-.38 2.65c-.61.25-1.17.59-1.69.98l-2.49-1c-.23-.09-.49 0-.61.22l-2 3.46c-.13.22-.07.49.12.64l2.11 1.65c-.04.32-.07.65-.07.98s.03.66.07.98l-2.11 1.65c-.19.15-.24.42-.12.64l2 3.46c.12.22.39.3.61.22l2.49-1c.52.4 1.08.73 1.69.98l.38 2.65c.03.24.24.42.49.42h4c.25 0 .46-.18.49-.42l.38-2.65c.61-.25 1.17-.59 1.69-.98l2.49 1c.23.09.49 0 .61-.22l2-3.46c.12-.22.07-.49-.12-.64l-2.11-1.65zM12 15.5c-1.93 0-3.5-1.57-3.5-3.5s1.57-3.5 3.5-3.5 3.5 1.57 3.5 3.5-1.57 3.5-3.5 3.5z" fill="white"/></svg>'
+            });
+
+            Lampa.Settings.add({
+                name: 'iptv_m3u_link',
+                section: 'iptv_lite_settings',
+                title: 'M3U Плейлист URL',
+                type: 'input',
+                placeholder: 'http://server.com/playlist.m3u',
+                default: ''
+            });
+
+            Lampa.Settings.add({
+                name: 'iptv_epg_link',
+                section: 'iptv_lite_settings',
+                title: 'XMLTV EPG URL',
+                type: 'input',
+                placeholder: 'http://server.com/epg.xml.gz',
+                default: ''
+            });
+        };
+
+        this.render = function (object) {
+            var m3u_url = storage.get('iptv_m3u_link', '');
+            var epg_url = storage.get('iptv_epg_link', '');
+            var scroll = new Lampa.Scroll({mask: true, over: true});
+            var items_container = $('<div class="category-full"></div>');
+            var active_group = null;
+
+            this.create = function () {
+                var _this = this;
+                if (!m3u_url) {
+                    items_container.append('<div class="empty">Укажите ссылку на M3U в настройках</div>');
+                    return [scroll.render()];
+                }
+
+                this.load(m3u_url);
+                return [scroll.render()];
+            };
+
+            this.load = function (url) {
+                var _this = this;
+                Lampa.Loading.show();
+                network.timeout(10000);
+                network.silent(url, function (str) {
+                    _this.parse(str);
+                    _this.showGroups();
+                    Lampa.Loading.hide();
+                }, function () {
+                    Lampa.Loading.hide();
+                    Lampa.Noty.show('Ошибка загрузки плейлиста');
+                }, false, {dataType: 'text'});
+            };
+
+            this.parse = function (str) {
+                playlistData = [];
+                groups = {'Все каналы': []};
+                var lines = str.split('\n');
+                var currentItem = null;
+
+                lines.forEach(function (line) {
+                    line = line.trim();
+                    if (line.indexOf('#EXTINF') === 0) {
+                        currentItem = {};
+                        var nameMatch = line.match(/,(.*)$/);
+                        var groupMatch = line.match(/group-title="([^"]+)"/);
+                        var logoMatch = line.match(/tvg-logo="([^"]+)"/);
+                        var idMatch = line.match(/tvg-id="([^"]+)"/);
+
+                        currentItem.name = nameMatch ? nameMatch[1] : 'Без названия';
+                        currentItem.group = groupMatch ? groupMatch[1] : 'Разное';
+                        currentItem.logo = logoMatch ? logoMatch[1] : '';
+                        currentItem.epg_id = idMatch ? idMatch[1] : '';
+                    } else if (line.indexOf('http') === 0 && currentItem) {
+                        currentItem.url = line;
+                        playlistData.push(currentItem);
+                        if (!groups[currentItem.group]) groups[currentItem.group] = [];
+                        groups[currentItem.group].push(currentItem);
+                        groups['Все каналы'].push(currentItem);
+                        currentItem = null;
+                    }
+                });
+            };
+
+            this.showGroups = function () {
+                var _this = this;
+                items_container.empty();
+                scroll.clear();
+                scroll.append(items_container);
+
+                Object.keys(groups).forEach(function (gName) {
+                    var item = Lampa.Template.get('button_category', {title: gName});
+                    item.on('hover:enter', function () {
+                        _this.showChannels(gName);
+                    });
+                    items_container.append(item);
+                });
+                Lampa.Controller.enable('content');
+            };
+
+            this.showChannels = function (gName) {
+                var _this = this;
+                items_container.empty();
+                
+                var back = Lampa.Template.get('button_category', {title: '[ Назад к группам ]'});
+                back.on('hover:enter', function () { _this.showGroups(); });
+                items_container.append(back);
+
+                groups[gName].forEach(function (chan) {
+                    var item = Lampa.Template.get('button_category', {title: chan.name});
+                    item.on('hover:enter', function () {
+                        var video = {
+                            url: chan.url,
+                            title: chan.name,
+                            callback: function() {
+                                Lampa.Controller.toggle('content');
+                            }
+                        };
+                        
+                        // Передаем данные для EPG в плеер
+                        Lampa.Player.play(video);
+                        Lampa.Player.playlist([video]);
+                        
+                        if (epg_url) {
+                            // Инъекция EPG id если Lampa поддерживает через доп. параметры
+                            video.tvg_id = chan.epg_id;
+                            video.xmltv = epg_url;
+                        }
+                    });
+                    items_container.append(item);
+                });
+                Lampa.Controller.enable('content');
+            };
+
+            this.pause = function () {};
+            this.stop = function () {};
+            this.destroy = function () {
+                items_container.remove();
+                scroll.destroy();
+            };
+        };
+    }
+
+    // Регистрация расширения
     Lampa.Extensions.add({
-        name: 'iptv_ott',
-        version: '1.0.1',
-
-        init: function () {
-            console.log('[IPTV OTT] init');
-
-            const KEY = 'iptv_ott_cfg';
-            let cfg = Lampa.Storage.get(KEY, { playlist: '', epg: '' });
-            const save = () => Lampa.Storage.set(KEY, cfg);
-
-            // ждём инициализации приложения
-            Lampa.Listener.follow('app', function () {
-
-                /* ===== SETTINGS ===== */
-
-                Lampa.Settings.add({
-                    title: 'IPTV (Ott style)',
-                    component: 'iptv_ott_settings'
-                });
-
-                Lampa.Component.add('iptv_ott_settings', {
-                    template:
-                        '<div class="settings">' +
-                            '<div class="settings__item selector" data-t="playlist">' +
-                                'URL плейлиста<div class="settings__value">' + (cfg.playlist || 'не задан') + '</div>' +
-                            '</div>' +
-                            '<div class="settings__item selector" data-t="epg">' +
-                                'URL EPG<div class="settings__value">' + (cfg.epg || 'не задан') + '</div>' +
-                            '</div>' +
-                        '</div>',
-                    start: function () {
-                        let root = this.render();
-                        root.find('.settings__item').on('click', function () {
-                            let t = this.dataset.t;
-                            Lampa.Input.edit({
-                                title: t === 'playlist' ? 'URL M3U' : 'URL EPG',
-                                value: cfg[t],
-                                onBack: v => { cfg[t] = v; save(); }
-                            });
-                        });
-                        Lampa.Controller.enable('content');
-                    }
-                });
-
-                /* ===== ENTRY ===== */
-
-                Lampa.Settings.add({
-                    title: 'IPTV (просмотр)',
-                    onClick: function () {
-                        if (!cfg.playlist) {
-                            Lampa.Noty.show('Укажи плейлист');
-                            return;
-                        }
-                        Lampa.Request.get(cfg.playlist, parseM3U);
-                    }
-                });
-
-                function parseM3U(text) {
-                    let list = [], cur = null;
-                    text.split('\n').forEach(l => {
-                        l = l.trim();
-                        if (l.startsWith('#EXTINF')) {
-                            cur = {
-                                title: l.split(',').pop(),
-                                group: (l.match(/group-title="(.*?)"/)||[])[1] || 'Без группы',
-                                logo: (l.match(/tvg-logo="(.*?)"/)||[])[1] || '',
-                                epg:  (l.match(/tvg-id="(.*?)"/)||[])[1] || '',
-                                url: ''
-                            };
-                        } else if (l && !l.startsWith('#') && cur) {
-                            cur.url = l;
-                            list.push(cur);
-                            cur = null;
-                        }
-                    });
-                    openUI(list);
+        id: 'iptv_lite',
+        type: 'plugin',
+        onInit: function () {
+            var plugin = new IPTVPlugin();
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type === 'ready') {
+                    plugin.init();
                 }
-
-                function openUI(channels) {
-                    Lampa.Activity.push({
-                        component: 'iptv_ott',
-                        title: 'IPTV',
-                        data: channels
-                    });
-                }
-
-                Lampa.Component.add('iptv_ott', {
-                    template:
-                        '<div class="iptv-ott">' +
-                            '<div class="iptv-groups"></div>' +
-                            '<div class="iptv-channels"></div>' +
-                        '</div>',
-                    start: function () {
-                        let root = this.render();
-                        let gbox = root.find('.iptv-groups');
-                        let cbox = root.find('.iptv-channels');
-
-                        let groups = {};
-                        this.params.data.forEach(c => {
-                            (groups[c.group] = groups[c.group] || []).push(c);
-                        });
-
-                        Object.keys(groups).forEach(g => {
-                            let el = document.createElement('div');
-                            el.className = 'selector';
-                            el.textContent = g;
-                            el.onclick = () => render(groups[g]);
-                            gbox[0].appendChild(el);
-                        });
-
-                        function render(list) {
-                            cbox.empty();
-                            list.forEach(ch => {
-                                let el = document.createElement('div');
-                                el.className = 'selector';
-                                el.textContent = ch.title;
-                                el.onclick = () => {
-                                    Lampa.Player.play({
-                                        title: ch.title,
-                                        url: ch.url,
-                                        type: 'tv',
-                                        epg: cfg.epg || null,
-                                        epg_id: ch.epg || null
-                                    });
-                                };
-                                cbox[0].appendChild(el);
-                            });
-                        }
-
-                        let first = Object.keys(groups)[0];
-                        if (first) render(groups[first]);
-                        Lampa.Controller.enable('content');
-                    }
-                });
-
             }, true);
         }
     });
