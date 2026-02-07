a75421ea3f22fe18243fd40c78d32750794edae9
Artrax90 <67760877+Artrax90@users.noreply.github.com>
2026-02-07 20:21:51 +0300

    Update iptv_ott.js

--- iptv_ott.js
+++ iptv_ott.js
@@ -1,15 +1,12 @@
 // ==Lampa==
 // name: IPTV Lite
-// version: 1.0.8
-// description: Simple IPTV player
+// version: 1.0.9
+// description: Исправлена ошибка render
 // author: Gemini
 // ==/Lampa==
 
 (function () {
     'use strict';
-
-    // Проверка наличия Lampa в глобальной области
-    if (typeof window.Lampa === 'undefined') return;
 
     function IPTVComponent(object) {
         var network = new Lampa.Reguest();
@@ -17,12 +14,13 @@
         var items = $('<div class="category-full"></div>');
         var groups = {};
 
+        // Метод create вызывается Lampa при открытии
         this.create = function () {
             var _this = this;
             var url = Lampa.Storage.get('iptv_m3u_link', '');
 
             if (!url) {
-                items.append('<div class="empty" style="text-align:center;padding:40px">Введите M3U ссылку в настройках</div>');
+                items.append('<div class="empty" style="text-align:center;padding:40px">Введите M3U ссылку в настройках IPTV Lite</div>');
             } else {
                 Lampa.Loading.show();
                 network.silent(url, function (str) {
@@ -36,7 +34,11 @@
             }
 
             scroll.append(items);
-            return [scroll.render()];
+        };
+
+        // ВАЖНО: Добавляем метод render, который запрашивает Lampa
+        this.render = function () {
+            return scroll.render();
         };
 
         this.parse = function (str) {
@@ -99,32 +101,40 @@
         };
     }
 
-    // Регистрация плагина (по канону lampaplugins/store)
     function plugin_init() {
+        // Регистрация компонента
         Lampa.Component.add('iptv_lite', IPTVComponent);
 
-        // Добавление в меню
-        var menu_item = $('<li class="menu__item selector" data-action="iptv_lite">' +
-            '<div class="menu__ico"><svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z" fill="white"/></svg></div>' +
-            '<div class="menu__text">IPTV Lite</div>' +
-            '</li>');
-
-        menu_item.on('hover:enter', function () {
-            Lampa.Activity.push({ title: 'IPTV Lite', component: 'iptv_lite', page: 1 });
+        // Кнопка в меню
+        Lampa.Menu.add({
+            id: 'iptv_lite',
+            title: 'IPTV Lite',
+            icon: '<svg height="36" viewBox="0 0 24 24" width="36" xmlns="http://www.w3.org/2000/svg"><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z" fill="white"/></svg>',
+            section: 'main',
+            onSelect: function () {
+                Lampa.Activity.push({ title: 'IPTV Lite', component: 'iptv_lite', page: 1 });
+            }
         });
 
-        $('.menu .menu__list').append(menu_item);
-
         // Настройки
-        Lampa.Params.select('iptv_m3u_link', '', 'M3U Link');
-        Lampa.Template.add('settings_iptv', '<div class="settings-param selector" data-name="iptv_m3u_link" data-type="input"><div class="settings-param__name">M3U Link</div><div class="settings-param__value"></div></div>');
-        
         Lampa.Settings.listener.follow('open', function (e) {
             if (e.name === 'main') {
                 var field = $('<div class="settings-folder selector"><div class="settings-folder__name">IPTV Lite Настройки</div></div>');
                 field.on('hover:enter', function () {
                     Lampa.Settings.create('iptv_lite_set', 'IPTV Lite');
-                    Lampa.Settings.add(Lampa.Template.get('settings_iptv', {}));
+                    
+                    var param = Lampa.Template.get('settings_param_input', {
+                        name: 'iptv_m3u_link',
+                        title: 'M3U Link',
+                        placeholder: 'http://...',
+                        value: Lampa.Storage.get('iptv_m3u_link', '')
+                    });
+
+                    param.on('change', function (v) {
+                        Lampa.Storage.set('iptv_m3u_link', v);
+                    });
+
+                    Lampa.Settings.add(param);
                     Lampa.Controller.enable('settings_iptv_lite_set');
                 });
                 e.body.find('.settings-main').append(field);
@@ -132,11 +142,14 @@
         });
     }
 
-    // Запуск без лишних проверок
-    if (window.app_ready) plugin_init();
-    else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') plugin_init();
+    if (window.Lampa) {
+        Lampa.Extensions.add({
+            id: 'iptv_lite_fixed',
+            type: 'plugin',
+            onInit: function () {
+                if (window.app_ready) plugin_init();
+                else Lampa.Listener.follow('app', function (e) { if (e.type === 'ready') plugin_init(); });
+            }
         });
     }
 })();
